088303bb25c2ca282ed9c131a15ecd64897ec2ef
aglawson <36459896+aglawson@users.noreply.github.com>
2021-02-03 10:33:27 -0800

    Compiles & updates storage contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.16;
+
+contract SimpleStorage {
+  string ipfsHash;
+
+  function set(string memory x) public {
+    ipfsHash = x;
+  }
+
+  function get() public view returns (string memory) {
+    return ipfsHash;
+  }
+}
