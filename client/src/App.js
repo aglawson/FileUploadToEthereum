7b469152ae7c377c6a125d4a64f745e32a3ddcfb
aglawson <36459896+aglawson@users.noreply.github.com>
2021-02-03 10:58:52 -0800

    configuring connections to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import ipfs from "./ipfs";
 
 import "./App.css";
 
@@ -39,7 +40,7 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods.set("5").send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call();
